b2bf0aad2549f1ede47bd70ff1757ab447a67932
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-03 16:00:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,3 +31,11 @@ function appendKitten(name) {
 function prependKitten(name) {
   return [name, ...kittens]
 }
+
+function removeLastKitten() {
+  return kittens.slice[0, kittens.length -1]
+}
+
+function removeFirstKitten() {
+  
+}
