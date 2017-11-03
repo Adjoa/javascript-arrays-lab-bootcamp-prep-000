20508f3c4b53799364ae887551b6f805d7e97c00
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-03 15:44:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const app = "I don't do much."
+
+function kittens() {
+  var kittens = ["Milo", "Otis", "Garfield"]
+}
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
